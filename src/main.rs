ddf62c68e344f23b43a9d80012db51f86e786a0c
lucasericsson222 <lucasericsson222@gmail.com>
2023-03-28 17:16:47 -0700

    Working enemy spawner

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 use bevy::{prelude::*, window::PrimaryWindow};
+use rand::prelude::*;
 
 pub const PLAYER_SIZE: f32 = 64.0; // This is the player sprite size. 
 pub const PLAYER_SPEED: f32 = 500.0;
+pub const NUMBER_OF_ENEMIES: usize = 5;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_startup_system(spawn_camera)
         .add_startup_system(spawn_player)
+        .add_startup_system(spawn_enemies)
         .add_system(player_movement)
         .add_system(confine_player_movement)
         .run()
@@ -15,6 +18,9 @@ fn main() {
 
 #[derive(Component)]
 pub struct Player {}
+
+#[derive(Component)]
+pub struct Enemy {}
 
 pub fn spawn_player(
     mut commands: Commands,
@@ -47,6 +53,30 @@ pub fn spawn_camera(
             ..default()
         }
     );
+}
+
+pub fn spawn_enemies(
+    mut commands: Commands,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    asset_server: Res<AssetServer>,
+) {
+    let window = window_query.get_single().unwrap();
+
+    for _ in 0..NUMBER_OF_ENEMIES {
+        let random_x = random::<f32>() * window.width();
+        let random_y = random::<f32>() * window.height();
+
+        commands.spawn(
+            (
+                SpriteBundle {
+                    transform: Transform::from_xyz(random_x, random_y, 0.0),
+                    texture: asset_server.load("sprites/ball_red_large.png"),
+                    ..default()
+                },
+                Enemy {},
+            )
+        );
+    }
 }
 
 pub fn player_movement(
